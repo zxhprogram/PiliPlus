guozhigq/pilipala

feat:支持排行榜功能

lib/models/common/rank_type.dart
@@ -1,0 +1,240 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:pilipala/pages/rank/zone/index.dart';
+
+enum RandType {
+  all,
+  creation,
+  animation,
+  music,
+  dance,
+  game,
+  knowledge,
+  technology,
+  sport,
+  car,
+  life,
+  food,
+  animal,
+  madness,
+  fashion,
+  entertainment,
+  film,
+  origin,
+  rookie
+}
+
+extension RankTypeDesc on RandType {
+  String get description => [
+        '全站',
+        '国创相关',
+        '动画',
+        '音乐',
+        '舞蹈',
+        '游戏',
+        '知识',
+        '科技',
+        '运动',
+        '汽车',
+        '生活',
+        '美食',
+        '动物圈',
+        '鬼畜',
+        '时尚',
+        '娱乐',
+        '影视'
+      ][index];
+
+  String get id => [
+        'all',
+        'creation',
+        'animation',
+        'music',
+        'dance',
+        'game',
+        'knowledge',
+        'technology',
+        'sport',
+        'car',
+        'life',
+        'food',
+        'animal',
+        'madness',
+        'fashion',
+        'entertainment',
+        'film'
+      ][index];
+}
+
+List tabsConfig = [
+  {
+    'icon': const Icon(
+      Icons.live_tv_outlined,
+      size: 15,
+    ),
+    'label': '全站',
+    'type': RandType.all,
+    'ctr': Get.put<ZoneController>,
+    'page': const ZonePage(rid: 0),
+  },
+  {
+    'icon': const Icon(
+      Icons.live_tv_outlined,
+      size: 15,
+    ),
+    'label': '国创相关',
+    'type': RandType.creation,
+    'ctr': Get.put<ZoneController>,
+    'page': const ZonePage(rid: 168),
+  },
+  {
+    'icon': const Icon(
+      Icons.live_tv_outlined,
+      size: 15,
+    ),
+    'label': '动画',
+    'type': RandType.animation,
+    'ctr': Get.put<ZoneController>,
+    'page': const ZonePage(rid: 1),
+  },
+  {
+    'icon': const Icon(
+      Icons.live_tv_outlined,
+      size: 15,
+    ),
+    'label': '音乐',
+    'type': RandType.music,
+    'ctr': Get.put<ZoneController>,
+    'page': const ZonePage(rid: 3),
+  },
+  {
+    'icon': const Icon(
+      Icons.live_tv_outlined,
+      size: 15,
+    ),
+    'label': '舞蹈',
+    'type': RandType.dance,
+    'ctr': Get.put<ZoneController>,
+    'page': const ZonePage(rid: 129),
+  },
+  {
+    'icon': const Icon(
+      Icons.live_tv_outlined,
+      size: 15,
+    ),
+    'label': '游戏',
+    'type': RandType.game,
+    'ctr': Get.put<ZoneController>,
+    'page': const ZonePage(rid: 4),
+  },
+  {
+    'icon': const Icon(
+      Icons.live_tv_outlined,
+      size: 15,
+    ),
+    'label': '知识',
+    'type': RandType.knowledge,
+    'ctr': Get.put<ZoneController>,
+    'page': const ZonePage(rid: 36),
+  },
+  {
+    'icon': const Icon(
+      Icons.live_tv_outlined,
+      size: 15,
+    ),
+    'label': '科技',
+    'type': RandType.technology,
+    'ctr': Get.put<ZoneController>,
+    'page': const ZonePage(rid: 188),
+  },
+  {
+    'icon': const Icon(
+      Icons.live_tv_outlined,
+      size: 15,
+    ),
+    'label': '运动',
+    'type': RandType.sport,
+    'ctr': Get.put<ZoneController>,
+    'page': const ZonePage(rid: 234),
+  },
+  {
+    'icon': const Icon(
+      Icons.live_tv_outlined,
+      size: 15,
+    ),
+    'label': '汽车',
+    'type': RandType.car,
+    'ctr': Get.put<ZoneController>,
+    'page': const ZonePage(rid: 223),
+  },
+  {
+    'icon': const Icon(
+      Icons.live_tv_outlined,
+      size: 15,
+    ),
+    'label': '生活',
+    'type': RandType.life,
+    'ctr': Get.put<ZoneController>,
+    'page': const ZonePage(rid: 160),
+  },
+  {
+    'icon': const Icon(
+      Icons.live_tv_outlined,
+      size: 15,
+    ),
+    'label': '美食',
+    'type': RandType.food,
+    'ctr': Get.put<ZoneController>,
+    'page': const ZonePage(rid: 211),
+  },
+  {
+    'icon': const Icon(
+      Icons.live_tv_outlined,
+      size: 15,
+    ),
+    'label': '动物圈',
+    'type': RandType.animal,
+    'ctr': Get.put<ZoneController>,
+    'page': const ZonePage(rid: 217),
+  },
+  {
+    'icon': const Icon(
+      Icons.live_tv_outlined,
+      size: 15,
+    ),
+    'label': '鬼畜',
+    'type': RandType.madness,
+    'ctr': Get.put<ZoneController>,
+    'page': const ZonePage(rid: 119),
+  },
+  {
+    'icon': const Icon(
+      Icons.live_tv_outlined,
+      size: 15,
+    ),
+    'label': '时尚',
+    'type': RandType.fashion,
+    'ctr': Get.put<ZoneController>,
+    'page': const ZonePage(rid: 155),
+  },
+  {
+    'icon': const Icon(
+      Icons.live_tv_outlined,
+      size: 15,
+    ),
+    'label': '娱乐',
+    'type': RandType.entertainment,
+    'ctr': Get.put<ZoneController>,
+    'page': const ZonePage(rid: 5),
+  },
+  {
+    'icon': const Icon(
+      Icons.live_tv_outlined,
+      size: 15,
+    ),
+    'label': '影视',
+    'type': RandType.film,
+    'ctr': Get.put<ZoneController>,
+    'page': const ZonePage(rid: 181),
+  }
+];

lib/pages/main/controller.dart
@@ -9,6 +9,7 @@ import 'package:pilipala/http/common.dart';
 import 'package:pilipala/pages/dynamics/index.dart';
 import 'package:pilipala/pages/home/view.dart';
 import 'package:pilipala/pages/media/index.dart';
+import 'package:pilipala/pages/rank/index.dart';
 import 'package:pilipala/utils/storage.dart';
 import 'package:pilipala/utils/utils.dart';
 import '../../models/common/dynamic_badge_mode.dart';
@@ -17,6 +18,7 @@ import '../../models/common/nav_bar_config.dart';
 class MainController extends GetxController {
   List<Widget> pages = <Widget>[
     const HomePage(),
+    const RankPage(),
     const DynamicsPage(),
     const MediaPage(),
   ];

lib/pages/main/view.dart
@@ -7,6 +7,7 @@ import 'package:pilipala/models/common/dynamic_badge_mode.dart';
 import 'package:pilipala/pages/dynamics/index.dart';
 import 'package:pilipala/pages/home/index.dart';
 import 'package:pilipala/pages/media/index.dart';
+import 'package:pilipala/pages/rank/index.dart';
 import 'package:pilipala/utils/event_bus.dart';
 import 'package:pilipala/utils/feed_back.dart';
 import 'package:pilipala/utils/storage.dart';
@@ -22,6 +23,7 @@ class MainApp extends StatefulWidget {
 class _MainAppState extends State<MainApp> with SingleTickerProviderStateMixin {
   final MainController _mainController = Get.put(MainController());
   final HomeController _homeController = Get.put(HomeController());
+  final RankController _rankController = Get.put(RankController());
   final DynamicsController _dynamicController = Get.put(DynamicsController());
   final MediaController _mediaController = Get.put(MediaController());
 
@@ -55,6 +57,21 @@ class _MainAppState extends State<MainApp> with SingleTickerProviderStateMixin {
       _homeController.flag = true;
     } else {
       _homeController.flag = false;
+    }
+
+    if (currentPage is RankPage) {
+      if (_rankController.flag) {
+        // 单击返回顶部 双击并刷新
+        if (DateTime.now().millisecondsSinceEpoch - _lastSelectTime! < 500) {
+          _rankController.onRefresh();
+        } else {
+          _rankController.animateToTop();
+        }
+        _lastSelectTime = DateTime.now().millisecondsSinceEpoch;
+      }
+      _rankController.flag = true;
+    } else {
+      _rankController.flag = false;
     }
 
     if (currentPage is DynamicsPage) {

lib/pages/rank/controller.dart
@@ -1,0 +1,70 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:hive/hive.dart';
+import 'package:pilipala/models/common/rank_type.dart';
+import 'package:pilipala/utils/storage.dart';
+
+class RankController extends GetxController with GetTickerProviderStateMixin {
+  bool flag = false;
+  late RxList tabs = [].obs;
+  RxInt initialIndex = 1.obs;
+  late TabController tabController;
+  late List tabsCtrList;
+  late List<Widget> tabsPageList;
+  Box setting = GStrorage.setting;
+  late final StreamController<bool> searchBarStream =
+      StreamController<bool>.broadcast();
+  late bool enableGradientBg;
+
+  @override
+  void onInit() {
+    super.onInit();
+    enableGradientBg =
+        setting.get(SettingBoxKey.enableGradientBg, defaultValue: true);
+    // 进行tabs配置
+    setTabConfig();
+  }
+
+  void onRefresh() {
+    int index = tabController.index;
+    var ctr = tabsCtrList[index];
+    ctr().onRefresh();
+  }
+
+  void animateToTop() {
+    int index = tabController.index;
+    var ctr = tabsCtrList[index];
+    ctr().animateToTop();
+  }
+
+  void setTabConfig() async {
+    tabs.value = tabsConfig;
+    initialIndex.value = 0;
+    tabsCtrList = tabs.map((e) => e['ctr']).toList();
+    tabsPageList = tabs.map<Widget>((e) => e['page']).toList();
+
+    tabController = TabController(
+      initialIndex: initialIndex.value,
+      length: tabs.length,
+      vsync: this,
+    );
+    // 监听 tabController 切换
+    if (enableGradientBg) {
+      tabController.animation!.addListener(() {
+        if (tabController.indexIsChanging) {
+          if (initialIndex.value != tabController.index) {
+            initialIndex.value = tabController.index;
+          }
+        } else {
+          final int temp = tabController.animation!.value.round();
+          if (initialIndex.value != temp) {
+            initialIndex.value = temp;
+            tabController.index = initialIndex.value;
+          }
+        }
+      });
+    }
+  }
+}

lib/pages/rank/zone/controller.dart
@@ -1,0 +1,53 @@
+import 'package:get/get.dart';
+import 'package:flutter/material.dart';
+import 'package:pilipala/http/video.dart';
+import 'package:pilipala/models/model_hot_video_item.dart';
+
+class ZoneController extends GetxController {
+  final ScrollController scrollController = ScrollController();
+  RxList<HotVideoItemModel> videoList = <HotVideoItemModel>[].obs;
+  bool isLoadingMore = false;
+  bool flag = false;
+  OverlayEntry? popupDialog;
+  int zoneID = 0;
+
+  // 获取推荐
+  Future queryRankFeed(type, rid) async {
+    zoneID = rid;
+    var res = await VideoHttp.getRankVideoList(zoneID);
+    if (res['status']) {
+      if (type == 'init') {
+        videoList.value = res['data'];
+      } else if (type == 'onRefresh') {
+        videoList.clear();
+        videoList.addAll(res['data']);
+      } else if (type == 'onLoad') {
+        videoList.clear();
+        videoList.addAll(res['data']);
+      }
+    }
+    isLoadingMore = false;
+    return res;
+  }
+
+  // 下拉刷新
+  Future onRefresh() async {
+    queryRankFeed('onRefresh', zoneID);
+  }
+
+  // 上拉加载
+  Future onLoad() async {
+    queryRankFeed('onLoad', zoneID);
+  }
+
+  // 返回顶部并刷新
+  void animateToTop() async {
+    if (scrollController.offset >=
+        MediaQuery.of(Get.context!).size.height * 5) {
+      scrollController.jumpTo(0);
+    } else {
+      await scrollController.animateTo(0,
+          duration: const Duration(milliseconds: 500), curve: Curves.easeInOut);
+    }
+  }
+}

zxhprogram/PiliPlus

lib/models/common/rank_type.dart
@@ -1,0 +1,240 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:PiliPalaX/pages/rank/zone/index.dart';
+
+enum RandType {
+  all,
+  creation,
+  animation,
+  music,
+  dance,
+  game,
+  knowledge,
+  technology,
+  sport,
+  car,
+  life,
+  food,
+  animal,
+  madness,
+  fashion,
+  entertainment,
+  film,
+  origin,
+  rookie
+}
+
+extension RankTypeDesc on RandType {
+  String get description => [
+        '全站',
+        '国创相关',
+        '动画',
+        '音乐',
+        '舞蹈',
+        '游戏',
+        '知识',
+        '科技',
+        '运动',
+        '汽车',
+        '生活',
+        '美食',
+        '动物圈',
+        '鬼畜',
+        '时尚',
+        '娱乐',
+        '影视'
+      ][index];
+
+  String get id => [
+        'all',
+        'creation',
+        'animation',
+        'music',
+        'dance',
+        'game',
+        'knowledge',
+        'technology',
+        'sport',
+        'car',
+        'life',
+        'food',
+        'animal',
+        'madness',
+        'fashion',
+        'entertainment',
+        'film'
+      ][index];
+}
+
+List tabsConfig = [
+  {
+    'icon': const Icon(
+      Icons.live_tv_outlined,
+      size: 15,
+    ),
+    'label': '全站',
+    'type': RandType.all,
+    'ctr': Get.put<ZoneController>,
+    'page': const ZonePage(rid: 0),
+  },
+  {
+    'icon': const Icon(
+      Icons.live_tv_outlined,
+      size: 15,
+    ),
+    'label': '国创相关',
+    'type': RandType.creation,
+    'ctr': Get.put<ZoneController>,
+    'page': const ZonePage(rid: 168),
+  },
+  {
+    'icon': const Icon(
+      Icons.live_tv_outlined,
+      size: 15,
+    ),
+    'label': '动画',
+    'type': RandType.animation,
+    'ctr': Get.put<ZoneController>,
+    'page': const ZonePage(rid: 1),
+  },
+  {
+    'icon': const Icon(
+      Icons.live_tv_outlined,
+      size: 15,
+    ),
+    'label': '音乐',
+    'type': RandType.music,
+    'ctr': Get.put<ZoneController>,
+    'page': const ZonePage(rid: 3),
+  },
+  {
+    'icon': const Icon(
+      Icons.live_tv_outlined,
+      size: 15,
+    ),
+    'label': '舞蹈',
+    'type': RandType.dance,
+    'ctr': Get.put<ZoneController>,
+    'page': const ZonePage(rid: 129),
+  },
+  {
+    'icon': const Icon(
+      Icons.live_tv_outlined,
+      size: 15,
+    ),
+    'label': '游戏',
+    'type': RandType.game,
+    'ctr': Get.put<ZoneController>,
+    'page': const ZonePage(rid: 4),
+  },
+  {
+    'icon': const Icon(
+      Icons.live_tv_outlined,
+      size: 15,
+    ),
+    'label': '知识',
+    'type': RandType.knowledge,
+    'ctr': Get.put<ZoneController>,
+    'page': const ZonePage(rid: 36),
+  },
+  {
+    'icon': const Icon(
+      Icons.live_tv_outlined,
+      size: 15,
+    ),
+    'label': '科技',
+    'type': RandType.technology,
+    'ctr': Get.put<ZoneController>,
+    'page': const ZonePage(rid: 188),
+  },
+  {
+    'icon': const Icon(
+      Icons.live_tv_outlined,
+      size: 15,
+    ),
+    'label': '运动',
+    'type': RandType.sport,
+    'ctr': Get.put<ZoneController>,
+    'page': const ZonePage(rid: 234),
+  },
+  {
+    'icon': const Icon(
+      Icons.live_tv_outlined,
+      size: 15,
+    ),
+    'label': '汽车',
+    'type': RandType.car,
+    'ctr': Get.put<ZoneController>,
+    'page': const ZonePage(rid: 223),
+  },
+  {
+    'icon': const Icon(
+      Icons.live_tv_outlined,
+      size: 15,
+    ),
+    'label': '生活',
+    'type': RandType.life,
+    'ctr': Get.put<ZoneController>,
+    'page': const ZonePage(rid: 160),
+  },
+  {
+    'icon': const Icon(
+      Icons.live_tv_outlined,
+      size: 15,
+    ),
+    'label': '美食',
+    'type': RandType.food,
+    'ctr': Get.put<ZoneController>,
+    'page': const ZonePage(rid: 211),
+  },
+  {
+    'icon': const Icon(
+      Icons.live_tv_outlined,
+      size: 15,
+    ),
+    'label': '动物圈',
+    'type': RandType.animal,
+    'ctr': Get.put<ZoneController>,
+    'page': const ZonePage(rid: 217),
+  },
+  {
+    'icon': const Icon(
+      Icons.live_tv_outlined,
+      size: 15,
+    ),
+    'label': '鬼畜',
+    'type': RandType.madness,
+    'ctr': Get.put<ZoneController>,
+    'page': const ZonePage(rid: 119),
+  },
+  {
+    'icon': const Icon(
+      Icons.live_tv_outlined,
+      size: 15,
+    ),
+    'label': '时尚',
+    'type': RandType.fashion,
+    'ctr': Get.put<ZoneController>,
+    'page': const ZonePage(rid: 155),
+  },
+  {
+    'icon': const Icon(
+      Icons.live_tv_outlined,
+      size: 15,
+    ),
+    'label': '娱乐',
+    'type': RandType.entertainment,
+    'ctr': Get.put<ZoneController>,
+    'page': const ZonePage(rid: 5),
+  },
+  {
+    'icon': const Icon(
+      Icons.live_tv_outlined,
+      size: 15,
+    ),
+    'label': '影视',
+    'type': RandType.film,
+    'ctr': Get.put<ZoneController>,
+    'page': const ZonePage(rid: 181),
+  }
+];

lib/pages/main/controller.dart
@@ -9,6 +9,7 @@ import 'package:PiliPalaX/http/common.dart';
 import 'package:PiliPalaX/pages/dynamics/index.dart';
 import 'package:PiliPalaX/pages/home/view.dart';
 import 'package:PiliPalaX/pages/media/index.dart';
+import 'package:PiliPalaX/pages/rank/index.dart';
 import 'package:PiliPalaX/utils/storage.dart';
 import 'package:PiliPalaX/utils/utils.dart';
 import '../../models/common/dynamic_badge_mode.dart';
@@ -17,6 +18,7 @@ import '../../models/common/nav_bar_config.dart';
 class MainController extends GetxController {
   List<Widget> pages = <Widget>[
     const HomePage(),
+    const RankPage(),
     const DynamicsPage(),
     const MediaPage(),
   ];

lib/pages/main/view.dart
@@ -7,6 +7,7 @@ import 'package:PiliPalaX/models/common/dynamic_badge_mode.dart';
 import 'package:PiliPalaX/pages/dynamics/index.dart';
 import 'package:PiliPalaX/pages/home/index.dart';
 import 'package:PiliPalaX/pages/media/index.dart';
+import 'package:PiliPalaX/pages/rank/index.dart';
 import 'package:PiliPalaX/utils/event_bus.dart';
 import 'package:PiliPalaX/utils/feed_back.dart';
 import 'package:PiliPalaX/utils/storage.dart';
@@ -22,6 +23,7 @@ class MainApp extends StatefulWidget {
 class _MainAppState extends State<MainApp> with SingleTickerProviderStateMixin {
   final MainController _mainController = Get.put(MainController());
   final HomeController _homeController = Get.put(HomeController());
+  final RankController _rankController = Get.put(RankController());
   final DynamicsController _dynamicController = Get.put(DynamicsController());
   final MediaController _mediaController = Get.put(MediaController());
 
@@ -55,6 +57,21 @@ class _MainAppState extends State<MainApp> with SingleTickerProviderStateMixin {
       _homeController.flag = true;
     } else {
       _homeController.flag = false;
+    }
+
+    if (currentPage is RankPage) {
+      if (_rankController.flag) {
+        // 单击返回顶部 双击并刷新
+        if (DateTime.now().millisecondsSinceEpoch - _lastSelectTime! < 500) {
+          _rankController.onRefresh();
+        } else {
+          _rankController.animateToTop();
+        }
+        _lastSelectTime = DateTime.now().millisecondsSinceEpoch;
+      }
+      _rankController.flag = true;
+    } else {
+      _rankController.flag = false;
     }
 
     if (currentPage is DynamicsPage) {

lib/pages/rank/controller.dart
@@ -1,0 +1,70 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:hive/hive.dart';
+import 'package:PiliPalaX/models/common/rank_type.dart';
+import 'package:PiliPalaX/utils/storage.dart';
+
+class RankController extends GetxController with GetTickerProviderStateMixin {
+  bool flag = false;
+  late RxList tabs = [].obs;
+  RxInt initialIndex = 1.obs;
+  late TabController tabController;
+  late List tabsCtrList;
+  late List<Widget> tabsPageList;
+  Box setting = GStrorage.setting;
+  late final StreamController<bool> searchBarStream =
+      StreamController<bool>.broadcast();
+  late bool enableGradientBg;
+
+  @override
+  void onInit() {
+    super.onInit();
+    enableGradientBg =
+        setting.get(SettingBoxKey.enableGradientBg, defaultValue: true);
+    // 进行tabs配置
+    setTabConfig();
+  }
+
+  void onRefresh() {
+    int index = tabController.index;
+    var ctr = tabsCtrList[index];
+    ctr().onRefresh();
+  }
+
+  void animateToTop() {
+    int index = tabController.index;
+    var ctr = tabsCtrList[index];
+    ctr().animateToTop();
+  }
+
+  void setTabConfig() async {
+    tabs.value = tabsConfig;
+    initialIndex.value = 0;
+    tabsCtrList = tabs.map((e) => e['ctr']).toList();
+    tabsPageList = tabs.map<Widget>((e) => e['page']).toList();
+
+    tabController = TabController(
+      initialIndex: initialIndex.value,
+      length: tabs.length,
+      vsync: this,
+    );
+    // 监听 tabController 切换
+    if (enableGradientBg) {
+      tabController.animation!.addListener(() {
+        if (tabController.indexIsChanging) {
+          if (initialIndex.value != tabController.index) {
+            initialIndex.value = tabController.index;
+          }
+        } else {
+          final int temp = tabController.animation!.value.round();
+          if (initialIndex.value != temp) {
+            initialIndex.value = temp;
+            tabController.index = initialIndex.value;
+          }
+        }
+      });
+    }
+  }
+}

lib/pages/rank/zone/controller.dart
@@ -1,0 +1,53 @@
+import 'package:get/get.dart';
+import 'package:flutter/material.dart';
+import 'package:PiliPalaX/http/video.dart';
+import 'package:PiliPalaX/models/model_hot_video_item.dart';
+
+class ZoneController extends GetxController {
+  final ScrollController scrollController = ScrollController();
+  RxList<HotVideoItemModel> videoList = <HotVideoItemModel>[].obs;
+  bool isLoadingMore = false;
+  bool flag = false;
+  OverlayEntry? popupDialog;
+  int zoneID = 0;
+
+  // 获取推荐
+  Future queryRankFeed(type, rid) async {
+    zoneID = rid;
+    var res = await VideoHttp.getRankVideoList(zoneID);
+    if (res['status']) {
+      if (type == 'init') {
+        videoList.value = res['data'];
+      } else if (type == 'onRefresh') {
+        videoList.clear();
+        videoList.addAll(res['data']);
+      } else if (type == 'onLoad') {
+        videoList.clear();
+        videoList.addAll(res['data']);
+      }
+    }
+    isLoadingMore = false;
+    return res;
+  }
+
+  // 下拉刷新
+  Future onRefresh() async {
+    queryRankFeed('onRefresh', zoneID);
+  }
+
+  // 上拉加载
+  Future onLoad() async {
+    queryRankFeed('onLoad', zoneID);
+  }
+
+  // 返回顶部并刷新
+  void animateToTop() async {
+    if (scrollController.offset >=
+        MediaQuery.of(Get.context!).size.height * 5) {
+      scrollController.jumpTo(0);
+    } else {
+      await scrollController.animateTo(0,
+          duration: const Duration(milliseconds: 500), curve: Curves.easeInOut);
+    }
+  }
+}