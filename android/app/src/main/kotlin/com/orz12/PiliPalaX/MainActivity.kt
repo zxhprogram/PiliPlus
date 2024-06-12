cf725e39d4690eb21b6ed7d221b03bb509cfd523
orz12 <ms_lyq@hotmail.com>
2024-06-12 18:32:28 +0800

    fix: 暗黑模式启动后隐藏状态栏有黑边

--- android/app/src/main/kotlin/com/orz12/PiliPalaX/MainActivity.kt
+++ android/app/src/main/kotlin/com/orz12/PiliPalaX/MainActivity.kt
@@ -4,6 +4,9 @@ import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
 import com.ryanheise.audioservice.AudioServiceActivity
+import android.os.Build
+import android.os.Bundle
+import android.view.WindowManager.LayoutParams
 
 class MainActivity : AudioServiceActivity() {
     private lateinit var methodChannel: MethodChannel
@@ -11,6 +14,14 @@ class MainActivity : AudioServiceActivity() {
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
         methodChannel = MethodChannel(flutterEngine!!.getDartExecutor()!!.getBinaryMessenger(), CHANNEL)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            window.attributes.layoutInDisplayCutoutMode =
+                LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES
+        }
     }
 
     override fun onUserLeaveHint() {
