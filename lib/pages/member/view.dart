7e8385661526c2a154e01163b1c6ca70889302f8
bggRGjQaUbCoE <githubaccount56556@proton.me>
2024-09-18 12:14:49 +0800

    fix: incorrect color for member title

--- lib/pages/member/view.dart
+++ lib/pages/member/view.dart
@@ -452,7 +452,7 @@ class _MemberPageState extends State<MemberPage>
                   ? '个人认证：'
                   : '机构认证：',
               style: TextStyle(
-                color: Theme.of(context).primaryColor,
+                color: Theme.of(context).colorScheme.primary,
               ),
               children: [
                 TextSpan(
