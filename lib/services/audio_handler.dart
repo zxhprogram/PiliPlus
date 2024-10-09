7167a751611e8c9b9f9ede4d15b9127a64f81a8f
bggRGjQaUbCoE <githubaccount56556@proton.me>
2024-10-09 14:02:16 +0800

    Revert "mod: exit"
    This reverts commit 659c7363d54b9162f8f9c9a3ba40bed1cb459a83.

--- lib/services/audio_handler.dart
+++ lib/services/audio_handler.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 import 'package:audio_service/audio_service.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:hive/hive.dart';
@@ -58,13 +56,6 @@ class VideoPlayerServiceHandler extends BaseAudioHandler with SeekHandler {
     ));
     await PlPlayerController.seekToIfExists(position, type: 'slider');
     // await player.seekTo(position);
-  }
-
-  @override
-  Future<void> onTaskRemoved() async {
-    if (Platform.isAndroid) {
-      exit(0);
-    }
   }
 
   Future<void> setMediaItem(MediaItem newMediaItem) async {
