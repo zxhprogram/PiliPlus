adf2188a1f006b697c5eca38039f660a518d72bf
bggRGjQaUbCoE <githubaccount56556@proton.me>
2024-10-13 09:42:55 +0800

    opt: dynamic: ontap

--- lib/pages/dynamics/widgets/dynamic_panel.dart
+++ lib/pages/dynamics/widgets/dynamic_panel.dart
@@ -42,7 +42,7 @@ class DynamicPanel extends StatelessWidget {
           borderRadius: BorderRadius.circular(5),
         ),
         child: InkWell(
-          onTap: source == 'detail'
+          onTap: source == 'detail' && item.type == 'DYNAMIC_TYPE_DRAW'
               ? null
               : () => _dynamicsController.pushDetail(item, 1),
           child: Column(
