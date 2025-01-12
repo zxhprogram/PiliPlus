a04da4c34ae0809b8902d32414292c2475f84d59
bggRGjQaUbCoE <githubaccount56556@proton.me>
2025-01-12 18:22:54 +0800

    fix: live qa btn
    Signed-off-by: bggRGjQaUbCoE <githubaccount56556@proton.me>

--- lib/pages/live_room/widgets/bottom_control.dart
+++ lib/pages/live_room/widgets/bottom_control.dart
@@ -105,28 +105,28 @@ class _BottomControlState extends State<BottomControl> {
             ),
             const SizedBox(width: 4),
           ],
-          SizedBox(
-            width: 30,
-            child: PopupMenuButton<int>(
-              padding: EdgeInsets.zero,
-              initialValue: widget.liveRoomCtr.currentQn,
-              onSelected: (value) {
-                widget.liveRoomCtr.changeQn(value);
-              },
-              child: Obx(
-                () => Text(
+          Obx(
+            () => SizedBox(
+              width: 30,
+              child: PopupMenuButton<int>(
+                padding: EdgeInsets.zero,
+                initialValue: widget.liveRoomCtr.currentQn,
+                onSelected: (value) {
+                  widget.liveRoomCtr.changeQn(value);
+                },
+                child: Text(
                   widget.liveRoomCtr.currentQnDesc.value,
                   style: const TextStyle(color: Colors.white, fontSize: 13),
                 ),
+                itemBuilder: (BuildContext context) {
+                  return widget.liveRoomCtr.acceptQnList.map((e) {
+                    return PopupMenuItem<int>(
+                      value: e['code'],
+                      child: Text(e['desc']),
+                    );
+                  }).toList();
+                },
               ),
-              itemBuilder: (BuildContext context) {
-                return widget.liveRoomCtr.acceptQnList.map((e) {
-                  return PopupMenuItem<int>(
-                    value: e['code'],
-                    child: Text(e['desc']),
-                  );
-                }).toList();
-              },
             ),
           ),
           const SizedBox(width: 10),
