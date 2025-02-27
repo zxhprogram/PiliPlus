acfa384c0ccb1bd3cbbd41899275b4c207d6bbd8
bggRGjQaUbCoE <githubaccount56556@proton.me>
2025-02-27 09:38:34 +0800

    fix: #333
    Signed-off-by: bggRGjQaUbCoE <githubaccount56556@proton.me>

--- lib/pages/video/detail/view_v.dart
+++ lib/pages/video/detail/view_v.dart
@@ -1804,11 +1804,7 @@ class _VideoDetailPageVState extends State<VideoDetailPageV>
           child: CustomScrollView(
             key: const PageStorageKey<String>('简介'),
             controller: needCtr ? _introController : null,
-            physics: needCtr.not
-                ? const NeverScrollableScrollPhysics(
-                    parent: ClampingScrollPhysics(),
-                  )
-                : null,
+            physics: needCtr.not ? const ClampingScrollPhysics() : null,
             slivers: [
               if (videoDetailController.videoType == SearchType.video) ...[
                 VideoIntroPanel(
