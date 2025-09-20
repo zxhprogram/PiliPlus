099c7b4dff27aadf97cfbd868730f106c6b68a4c
Kofua <1638183271zjn@gmail.com>
2025-09-20 11:08:33 +0800

    fix small black flicker window on startup for macOS (#1287)

--- macos/Runner/MainFlutterWindow.swift
+++ macos/Runner/MainFlutterWindow.swift
@@ -4,12 +4,26 @@ import FlutterMacOS
 class MainFlutterWindow: NSWindow {
   override func awakeFromNib() {
     let flutterViewController = FlutterViewController.init()
-    let windowFrame = self.frame
+    // 先不显示窗口
+    self.isReleasedWhenClosed = false
     self.contentViewController = flutterViewController
-    self.setFrame(windowFrame, display: true)
+    self.setFrame(self.frame, display: true)
+
+    // 背景别用默认黑色
+    self.isOpaque = false
+    self.backgroundColor = .clear
 
     RegisterGeneratedPlugins(registry: flutterViewController)
 
+    // 监听首帧渲染完成再显示窗口
+    NotificationCenter.default.addObserver(
+      forName: NSNotification.Name("io.flutter.embedding.engine.firstFrame"),
+      object: flutterViewController.engine, queue: .main
+    ) { [weak self] _ in
+      self?.makeKeyAndOrderFront(nil)
+      NSApp.activate(ignoringOtherApps: true)
+    }
+    // 不在这里调用 makeKeyAndOrderFront
     super.awakeFromNib()
   }
 }
